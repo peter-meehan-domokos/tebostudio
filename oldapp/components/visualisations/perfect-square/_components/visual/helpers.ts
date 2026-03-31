434700d2bf4134e0beea7754e8c7e8b10896e0ee
Peter Domokos <petedomokos@gmail.com>
2026-03-31 18:07:51 +0100

    removed oldapp folder

--- oldapp/components/visualisations/perfect-square/_components/visual/helpers.ts
+++
@@ -1,7 +0,0 @@
-import { ArrangeBy } from "../perfect-square/types";
-
-export const _simulationIsOn = (arrangeBySettings : ArrangeBy | null):boolean => {
-    if(!arrangeBySettings) { return false; }
-    const { x, y } = arrangeBySettings;
-    return x || y ? true : false;
-}
